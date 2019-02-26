45db1a83445eba458b20bdbbd04951de674ff7ac
Veeranjaneyulu Gorantla <vegorant@cisco.com>
2019-02-26 05:11:35 -0800

    No changes;

--- Linux/scripts/tmux/connect_multiple_hosts.sh
+++ Linux/scripts/tmux/connect_multiple_hosts.sh
@@ -1,14 +1,12 @@
 ## USAGE:
 ## sh connect_multiple_hosts.sh sla-blr1-1-7 sla-aer1-1-7 sla-rch1-1-7 sla-aln1-1-7 sla-rtp1-1-7 sla-rtp5-1-7 sla-sjc1-1-7 sla-mtv1-1-7
 
-starttmux() {
+function starttmux() {
     if [ -z "$HOSTS" ]; then
-       echo -n "Please provide of list of hosts separated by spaces [ENTER]: "
-       read HOSTS
+        echo -n "Please provide of list of hosts separated by spaces [ENTER]: "
+        read HOSTS
     fi
-
     local hosts=( $HOSTS )
-
     tmux new-window "ssh ${hosts[0]}"
     unset hosts[0];
     for i in "${hosts[@]}"; do
@@ -17,7 +15,6 @@ starttmux() {
     done
     tmux select-pane -t 0
     tmux set-window-option synchronize-panes on > /dev/null
-
 }
 
 HOSTS=${HOSTS:=$*}
